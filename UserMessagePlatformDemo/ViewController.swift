0bb2f515dea929b29510de6ca4396f0e4bfdc6b8
arimura <33759663+arimura@users.noreply.github.com>
2022-03-14 22:15:20 +0900

    Get consent from users

--- UserMessagePlatformDemo/ViewController.swift
+++ UserMessagePlatformDemo/ViewController.swift
@@ -9,30 +9,65 @@ import UIKit
 import UserMessagingPlatform
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-      
+        UMPConsentInformation.sharedInstance.reset()
+        
         // Create a UMPRequestParameters object.
         let parameters = UMPRequestParameters()
+        let debugSettings = UMPDebugSettings()
+        debugSettings.testDeviceIdentifiers = ["TEST-DEVICE-HASHED-ID"]
+        debugSettings.geography = UMPDebugGeography.EEA
+        parameters.debugSettings = debugSettings
+        
         // Set tag for under age of consent. Here false means users are not under age.
         parameters.tagForUnderAgeOfConsent = false
-
+        
         // Request an update to the consent information.
         UMPConsentInformation.sharedInstance.requestConsentInfoUpdate(
             with: parameters,
             completionHandler: { error in
-              if error != nil {
-                // Handle the error.
-              } else {
-                // The consent information state was updated.
-                // You are now ready to check if a form is
-                // available.
-              }
+                if error != nil {
+                    // Handle the error.
+                    print("error: \(error)")
+                } else {
+                    // The consent information state was updated.
+                    // You are now ready to check if a form is
+                    // available.
+                    let formStatus = UMPConsentInformation.sharedInstance.formStatus
+                    if formStatus == UMPFormStatus.available {
+                        self.loadForm()
+                    }
+                }
             })
     }
-
-
+    
+    func loadForm() {
+        UMPConsentForm.load(completionHandler: { form, loadError in
+            if loadError != nil {
+                // Handle the error.
+                print("error: \(loadError)")
+            } else {
+                // Present the form. You can also hold on to the reference to present
+                // later.
+                if UMPConsentInformation.sharedInstance.consentStatus == UMPConsentStatus.required {
+                    form?.present(
+                        from: self,
+                        completionHandler: { dismissError in
+                            if UMPConsentInformation.sharedInstance.consentStatus == UMPConsentStatus.obtained {
+                                // App can start requesting ads.
+                            }
+                            
+                        })
+                } else {
+                    // Keep the form available for changes to user consent.
+                    print("not required")
+                }
+            }
+        })
+    }
+    
 }
 
