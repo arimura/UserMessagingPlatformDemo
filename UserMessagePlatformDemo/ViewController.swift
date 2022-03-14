8877df454de07b3e9e7898b6e40257285fba9b9f
arimura <33759663+arimura@users.noreply.github.com>
2022-03-14 20:12:59 +0900

    Init

---
+++ UserMessagePlatformDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserMessagePlatformDemo
+//
+//  Created by 有村皓太郎 on 2022/03/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
